9c45e62000e4d33969babc073461d692e396bfb2
Diem-Hoang <trannguyendiemhoang@gmail.com>
2022-05-19 12:30:55 +0700

    add

---
+++ module_3/ss09/src/main/java/ServletDiscountServlet.java
@@ -0,0 +1,33 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ServletDiscountServlet", value = "/display-discount")
+public class ServletDiscountServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String description = request.getParameter("description");
+        int price = Integer.parseInt(request.getParameter("price"));
+        int percent = Integer.parseInt(request.getParameter("percent"));
+        double discount_amount = (price * percent * 0.01);
+        double discount_price = price - discount_amount;
+
+        request.setAttribute("description", description);
+        request.setAttribute("price", price);
+        request.setAttribute("percent", percent);
+        request.setAttribute("discount_amount", discount_amount);
+        request.setAttribute("discount_price", discount_price);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("result.jsp");
+        dispatcher.forward(request,response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ module_3/ss09/src/main/webapp/result.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<body>
+
+<h1 style="margin-top: 30px; color: cornflowerblue; text-align: center; font-weight: 100">Product Discount Calculator</h1>
+<div class="alert alert-success" role="alert" style="margin: 100px">
+    <p>Product Description: ${description}</p>
+    <p>List Price: ${price}</p>
+    <p>Discount Percent: ${percent}</p>
+    <hr>
+    <p>Discount Amount: ${discount_amount}</p>
+    <p>Discount Price: ${discount_price}</p>
+</div>
+</body>
+</html>
